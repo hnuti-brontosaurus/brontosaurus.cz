c070ecb93f5125b39245a3e21a2079815c56f02f
Daniel Kurowski <daniel.kurowski@grifart.cz>
2021-11-02 10:49:24 +0100

    Zapoj se: skeleton

---
+++ template-parts/content/content-zapoj-se.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+?>
+
+<main class="zapoj-se" role="main" id="obsah">
+	<section>
+		<h1>Aktuální příležitosti</h1>
+	</section>
+
+	<section>
+		<h1>Chceš se zapojit? Ozvi se nám!</h1>
+	</section>
+</main>
+
+<aside>
+	<section class="related">
+		<h1 class="related-heading">
+			Ještě by&nbsp;tě mohlo zajímat
+		</h1>
+	</section>
+</aside>
+
+<script src="<?php echo $viewAssetsPath; ?>/dist/js/index.js" type="text/javascript"></script><?php // maybe rather by using add_action('wp_enqueue_scripts') ?>
