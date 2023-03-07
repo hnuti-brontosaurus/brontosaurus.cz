ca477a0709303762338fe75f8b314df051bbf529
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-03-07 13:47:18 +0100

    cs

--- UI/EventDetail/EventDetailController.php
+++ UI/EventDetail/EventDetailController.php
@@ -14,6 +14,16 @@ use HnutiBrontosaurus\Theme\UI\DataContainers\Events\EventDC;
 use Latte\Engine;
 use Nette\Http\Request;
 use Nette\Utils\Strings;
+use function add_action;
+use function array_merge;
+use function get_query_var;
+use function parse_url;
+use function preg_match;
+use function sprintf;
+use function str_replace;
+use function wp_enqueue_script;
+use function wp_get_theme;
+use const PHP_URL_HOST;
 
 
 final class EventDetailController implements Controller
@@ -31,37 +41,7 @@ final class EventDetailController implements Controller
 		private Base $base,
 		private Engine $latte,
 		private Request $httpRequest,
-	) {
-		$this->latte->addFilter('renderWebsiteUserFriendly', static function (string $website): string {
-			$hostname = \parse_url($website, PHP_URL_HOST);
-			$hostname = $hostname !== null ? $hostname : $website; // in case of passing "a.b.cz" to parse_url(), it fails to parse it for some reason, so just skip it
-
-			if (Strings::startsWith($hostname, 'www.')) {
-				$hostname = \str_replace('www.', '', $hostname);
-			}
-
-			return $hostname;
-		});
-
-		$this->latte->addFilter('resolveRegistrationLink', function (EventDC $event): string
-		{
-			return $this->applicationUrlTemplate->for($event->id);
-		});
-
-		$this->latte->addFilter('formatDayCount', static fn(int $days): string =>
-			match($days) {
-				1 => \sprintf('%d den', $days),
-				2,3,4 => \sprintf('%d dny', $days),
-				default => \sprintf('%d dní', $days),
-		});
-
-		$this->latte->addFilter('formatHourCount', static fn(int $hours): string =>
-			match($hours) {
-				1 => \sprintf('%d hodinu', $hours),
-				2,3,4 => \sprintf('%d hodiny', $hours),
-				default => \sprintf('%d hodin', $hours),
-		});
-	}
+	) {}
 
 
 	private Event $event;
@@ -71,6 +51,8 @@ final class EventDetailController implements Controller
 	{
 		$eventId = (int) get_query_var(self::PARAM_EVENT_ID);
 
+		$this->registerFilters();
+
 		$hasBeenUnableToLoad = false;
 
 		try {
@@ -78,11 +60,10 @@ final class EventDetailController implements Controller
 			$eventDC = new EventDC($this->event, $this->dateFormatHuman, $this->dateFormatRobot);
 
 			// add event name to title tag (source https://stackoverflow.com/a/62410632/3668474)
-			add_filter('document_title_parts', function (array $title) {
-				return \array_merge($title, [
-					'title' => $this->event->getName(),
-				]);
-			});
+			add_filter(
+				'document_title_parts',
+				fn(array $title) => array_merge($title, ['title' => $this->event->getName()]),
+			);
 
 		} catch (EventNotFound) {
 			throw new NotFound();
@@ -92,7 +73,7 @@ final class EventDetailController implements Controller
 		}
 
 //		$parentPageSlug = get_query_var(self::PARAM_PARENT_PAGE_SLUG); // does not work for some reason
-		\preg_match('/^\/([\w-]+).*/', $this->httpRequest->getUrl()->getPath(), $matches);
+		preg_match('/^\/([\w-]+).*/', $this->httpRequest->getUrl()->getPath(), $matches);
 		$parentPageSlug = $matches[1];
 		$params = [
 			'event' => $eventDC,
@@ -112,8 +93,40 @@ final class EventDetailController implements Controller
 
 		$this->latte->render(
 			__DIR__ . '/EventDetailController.latte',
-			\array_merge($this->base->getLayoutVariables('detail'), $params),
+			array_merge($this->base->getLayoutVariables('detail'), $params),
 		);
+	}
+
+
+	private function registerFilters(): void
+	{
+		$this->latte->addFilter('renderWebsiteUserFriendly', static function (string $website): string {
+			$hostname = parse_url($website, PHP_URL_HOST);
+			$hostname = $hostname !== null ? $hostname : $website; // in case of passing "a.b.cz" to parse_url(), it fails to parse it for some reason, so just skip it
+
+			if (Strings::startsWith($hostname, 'www.')) {
+				$hostname = str_replace('www.', '', $hostname);
+			}
+
+			return $hostname;
+		});
+
+		$this->latte->addFilter('resolveRegistrationLink', fn(EventDC $event): string =>
+			$this->applicationUrlTemplate->for($event->id));
+
+		$this->latte->addFilter('formatDayCount', static fn(int $days): string =>
+			match($days) {
+				1 => sprintf('%d den', $days),
+				2,3,4 => sprintf('%d dny', $days),
+				default => sprintf('%d dní', $days),
+			});
+
+		$this->latte->addFilter('formatHourCount', static fn(int $hours): string =>
+			match($hours) {
+				1 => sprintf('%d hodinu', $hours),
+				2,3,4 => sprintf('%d hodiny', $hours),
+				default => sprintf('%d hodin', $hours),
+			});
 	}
 
 }
