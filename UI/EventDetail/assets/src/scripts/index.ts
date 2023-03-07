da0093577e8199ff65749cfc1f3fb50c4b36fa88
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-03-07 13:05:06 +0100

    remove application from scripts

--- UI/EventDetail/assets/src/scripts/index.ts
+++ UI/EventDetail/assets/src/scripts/index.ts
@@ -1,9 +1,5 @@
 import handleflashMessage from './handleFlashMessage';
-import handleRegistrationForm from './Registration/index';
-import {scrollToErrorIfAny} from './scrollToErrorIfAny';
 
 document.addEventListener('DOMContentLoaded', () => {
 	handleflashMessage(document);
-	handleRegistrationForm(window, document); // it does not contain any images thus it is safe to trigger on DOMContentLoaded
-	scrollToErrorIfAny(document);
 });
