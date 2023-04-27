485554854ef0d0ea449245a9fa2fb2284f54d0bf
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-04-27 12:31:03 +0200

    EventDC: make properties public so that it's better readable

--- UI/DataContainers/Events/EventDC.php
+++ UI/DataContainers/Events/EventDC.php
@@ -3,80 +3,48 @@
 namespace HnutiBrontosaurus\Theme\UI\DataContainers\Events;
 
 use Brick\DateTime\LocalDate;
+use DateTimeImmutable;
 use HnutiBrontosaurus\BisClient\Event\Category;
 use HnutiBrontosaurus\BisClient\Event\Group;
 use HnutiBrontosaurus\BisClient\Event\IntendedFor;
 use HnutiBrontosaurus\BisClient\Event\Response\Event;
 use HnutiBrontosaurus\Theme\UI\Event\EventController;
-use HnutiBrontosaurus\Theme\UI\PropertyHandler;
 use HnutiBrontosaurus\Theme\UI\Utils;
 use function get_site_url;
+use function implode;
 use function rtrim;
 use function sprintf;
 
 
-/**
- * @property-read int $id
- * @property-read string $link
- * @property-read string $title
- * @property-read string $dateStartForHumans
- * @property-read string $dateStartForRobots
- * @property-read bool $hasTimeStart
- * @property-read string|null $timeStart
- * @property-read \DateTimeImmutable $dateEnd
- * @property-read string $dateSpan
- * @property-read int $duration
- * @property-read PlaceDC $place
- * @property-read AgeDC $age
- * @property-read bool $isPaid
- * @property-read string|null $price
- * @property-read ContactDC $contact
- * @property-read bool $isRegistrationRequired
- * @property-read bool $isFull
- * @property-read bool $isForFirstTimeAttendees
- * @property-read InvitationDC $invitation
- * @property-read bool $areOrganizersListed
- * @property-read string|null $organizers
- * @property-read string|NULL $organizerUnit
- * @property-read bool $hasCoverPhoto
- * @property-read string|null $coverPhotoPath
- * @property-read ProgramDC $program
- * @property-read bool $hasRelatedWebsite
- * @property-read string|null $relatedWebsite
- * @property-read Tag[] $tags
- */
-final class EventDC
+final /*readonly*/ class EventDC
 {
-	use PropertyHandler;
-
-
-	private int $id;
-	private string $link;
-	private string $title;
-	private bool $hasCoverPhoto;
-	private ?string $coverPhotoPath;
-	private string $dateStartForHumans;
-	private string $dateStartForRobots;
-	private bool $hasTimeStart;
-	private ?string $timeStart;
-	private \DateTimeImmutable $dateEnd;
-	private string $dateSpan;
-	private int $duration;
-	private PlaceDC $place;
-	private AgeDC $age;
-	private bool $isPaid;
-	private ?string $price;
-	private ContactDC $contact;
-	private bool $isRegistrationRequired;
-	private bool $isFull;
-	private bool $isForFirstTimeAttendees;
-	private InvitationDC $invitation;
-	private bool $areOrganizersListed;
-	private ?string $organizers;
-	private ?string $organizerUnit;
-	private ProgramDC $program;
-	private bool $hasRelatedWebsite;
-	private ?string $relatedWebsite;
+	public int $id;
+	public string $link;
+	public string $title;
+	public bool $hasCoverPhoto;
+	public ?string $coverPhotoPath;
+	public string $dateStartForHumans;
+	public string $dateStartForRobots;
+	public bool $hasTimeStart;
+	public ?string $timeStart;
+	public DateTimeImmutable $dateEnd;
+	public string $dateSpan;
+	public int $duration;
+	public PlaceDC $place;
+	public AgeDC $age;
+	public bool $isPaid;
+	public ?string $price;
+	public ContactDC $contact;
+	public bool $isRegistrationRequired;
+	public bool $isFull;
+	public bool $isForFirstTimeAttendees;
+	public InvitationDC $invitation;
+	public bool $areOrganizersListed;
+	public ?string $organizers;
+	public ?string $organizerUnit;
+	public ProgramDC $program;
+	public bool $hasRelatedWebsite;
+	public ?string $relatedWebsite;
 	/** @var Tag[] */
 	public array $tags = [];
 
@@ -123,7 +91,7 @@ final class EventDC
 		$organizers = $event->getPropagation()->getOrganizers();
 		$this->areOrganizersListed = $organizers !== null;
 		$this->organizers = $organizers;
-		$this->organizerUnit = \implode(', ', $event->getAdministrationUnits());
+		$this->organizerUnit = implode(', ', $event->getAdministrationUnits());
 
 		$this->duration = $event->getDuration();
 
@@ -175,7 +143,7 @@ final class EventDC
 		$inSameMonth = $dateFrom->format('n') === $dateUntil->format('n');
 		$inSameYear = $dateFrom->format('Y') === $dateUntil->format('Y');
 
-		$dateSpan_fromPart = $dateFrom->format(\sprintf('j.%s%s',
+		$dateSpan_fromPart = $dateFrom->format(sprintf('j.%s%s',
 			( ! $inSameMonth || ! $inSameYear) ? ' n.' : '',
 			( ! $inSameYear) ? ' Y' : ''
 		));
@@ -186,7 +154,7 @@ final class EventDC
 			$optionalSpace = ' ';
 		}
 
-		return $dateSpan_fromPart . \sprintf('%s–%s', $optionalSpace, $optionalSpace) . $dateSpan_untilPart;
+		return $dateSpan_fromPart . sprintf('%s–%s', $optionalSpace, $optionalSpace) . $dateSpan_untilPart;
 	}
 
 }
