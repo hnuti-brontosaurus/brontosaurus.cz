ce70bbe4c8cf50562cd422173758c1095cc7e3fc
Daniel Kurowski <daniel.kurowski@grifart.cz>
2023-05-17 16:23:33 +0200

    comment

--- frontend/src/scripts/editor.js
+++ frontend/src/scripts/editor.js
@@ -1,4 +1,5 @@
 // overwrites core/heading block to be aligned to center by default
+// source https://stackoverflow.com/a/73676310
 wp.blocks.registerBlockVariation('core/heading', {
 	name: 'heading-center',
 	title: 'Nadpis',
