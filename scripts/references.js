2f7d28d2e23121ae0b1c1136136f2332dad4658b
dakur <dev@kurowsti.cz>
2025-04-03 21:09:15 +0200

    fix ratio problem

--- scripts/references.js
+++ scripts/references.js
@@ -96,7 +96,8 @@ class ElementsInView {
     count() {
         const containerWidth = this.slidesEl.getBoundingClientRect().width;
         const slideWidth = this.slidesEl.children[0].getBoundingClientRect().width;
-        const count = Math.floor(containerWidth / slideWidth);
+        const ratio = containerWidth / slideWidth;
+        const count = ratio < 1 ? 1 : Math.floor(ratio);
         return count;
     }
 
