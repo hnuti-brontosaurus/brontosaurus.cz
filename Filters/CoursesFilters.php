197ca665f5746e668458c4d26d20bcbb4249293e
dakur <dev@kurowsti.cz>
2026-02-10 20:53:09 +0100

    reflect changes in BIS categories

--- Filters/CoursesFilters.php
+++ Filters/CoursesFilters.php
@@ -8,9 +8,8 @@ use HnutiBrontosaurus\BisClient\Event\Request\EventParameters;
 
 final class CoursesFilters
 {
-	const FILTER_TALKS = 'prednasky';
 	const FILTER_ORGANIZING = 'organizatorske-kurzy';
-	const FILTER_THEMATIC = 'tematicke-kurzy';
+	const FILTER_THEMATIC = 'tematicke-kurzy-a-prednasky';
 
 	private static EventParameters $parameters;
 
@@ -24,19 +23,12 @@ final class CoursesFilters
 		}
 
 		switch ($selectedFilter) {
-			case self::FILTER_TALKS:
-				$parameters->setCategories([
-					Category::EDUCATIONAL_LECTURE(),
-					Category::CLUB_LECTURE(),
-				]);
-				break;
-
 			case self::FILTER_ORGANIZING:
-				$parameters->setCategory(Category::EDUCATIONAL_OHB());
+				$parameters->setCategory(Category::INTERNAL_EDUCATIONAL());
 				break;
 
 			case self::FILTER_THEMATIC:
-				$parameters->setCategory(Category::EDUCATIONAL_COURSE());
+				$parameters->setCategory(Category::PUBLIC_EDUCATIONAL());
 				break;
 		}
 	}
@@ -51,12 +43,8 @@ final class CoursesFilters
 	private static function allRelevantTypes(): void
 	{
 		self::$parameters->setCategories([
-			Category::EDUCATIONAL_LECTURE(),
-			Category::EDUCATIONAL_COURSE(),
-			Category::EDUCATIONAL_OHB(),
-			Category::EDUCATIONAL_EDUCATIONAL(),
-			Category::EDUCATIONAL_EDUCATIONAL_WITH_STAY(),
-			Category::CLUB_LECTURE(),
+			Category::INTERNAL_EDUCATIONAL(),
+			Category::PUBLIC_EDUCATIONAL(),
 		]);
 	}
 
