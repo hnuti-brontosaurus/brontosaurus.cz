d344f5ed28d7f18bc7c326b897dbee96a40b8bab
dakur <dev@kurowsti.cz>
2026-03-28 14:53:14 +0000

    simplify gulp build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,55 +2,71 @@
 
 import gulp from 'gulp';
 import taskList from 'gulp-task-listing';
+import autoprefixer from 'gulp-autoprefixer';
+import plumber from 'gulp-plumber';
+import rename from 'gulp-rename';
+import gulpSass from 'gulp-sass';
+import * as sassPackage from 'sass';
+const sass = gulpSass(sassPackage);
+import sourcemaps from 'gulp-sourcemaps';
 
-import buildStyles from './gulp.build-styles.js';
-
-const paths = {
-	styles: { global: { dist: 'frontend/dist/css', src: 'frontend/src/styles' } },
+const stylesPath = {
+	dist: 'frontend/dist/css', 
+	src: 'frontend/src/styles',
 };
-
-/**
- *
- * @param paths Expects paths.[type] object from ./constants.js
- * @returns {Array}
- */
-export const getSourcePathFromPathObject = (paths) => {
-	let sourcePaths = [];
-	for (let type in paths) {
-		const location = paths[type];
-		sourcePaths.push(location.src);
-	}
-
-	return sourcePaths;
-};
-
 
 
 // STYLES
 
 const buildStylesTask = (cb) => {
-	buildStyles(cb, [
+	const files = [
 		{
-			distPath: paths.styles.global.dist, // folder to save the compiled css file into
+			distPath: stylesPath.dist, // folder to save the compiled css file into
 			distFileName: 'style.css',
-			sourceFileName: paths.styles.global.src + '/style.scss', // name of source file
+			sourceFileName: stylesPath.src + '/style.scss', // name of source file
 		},
 		{
-			distPath: paths.styles.global.dist, // folder to save the compiled css file into
+			distPath: stylesPath.dist, // folder to save the compiled css file into
 			distFileName: 'editor.css',
-			sourceFileName: paths.styles.global.src + '/editor.scss', // name of source file
+			sourceFileName: stylesPath.src + '/editor.scss', // name of source file
 		},
 		{
-			distPath: paths.styles.global.dist, // folder to save the compiled css file into
+			distPath: stylesPath.dist, // folder to save the compiled css file into
 			distFileName: 'events.css',
-			sourceFileName: paths.styles.global.src + '/events.scss', // name of source file
+			sourceFileName: stylesPath.src + '/events.scss', // name of source file
 		},
-	]);
+	];
+
+	for (let i in files) {
+		let file = files[i];
+
+		if (!file.distFileName || !file.distPath || !file.sourceFileName) {
+			throw new Error('`distFileName`, `distPath` and `sourceFileName` has to be defined for each item.');
+		}
+
+		gulp.src(file.sourceFileName)
+			.pipe(plumber())
+			.pipe(rename(file.distFileName)) // must be before sass to keep correct file names between css and sourcemap
+			.pipe(sourcemaps.init())
+			.pipe(sass({
+				noCache: true,
+				outputStyle: (process.env.NODE_ENV === 'production' ? 'compressed' : 'expanded')
+			}))
+			.pipe(autoprefixer()) // retrieves from `.browserslistrc`
+			.pipe(sourcemaps.write('.'))
+			.pipe(plumber.stop())
+			.pipe(gulp.dest(file.distPath))
+			.on('end', () => {
+				if (parseInt(i) === (files.length-1)) { // call only on last pipe
+					cb();
+				}
+			});
+	}
 };
 
 gulp.task('build:styles', gulp.series(buildStylesTask));
 gulp.task('watch:styles', gulp.series(buildStylesTask, () => {
-	gulp.watch(getSourcePathFromPathObject(paths.styles).map((path) => path + '/**/*.scss'), gulp.series(buildStylesTask));
+	gulp.watch(stylesPath.src + '/**/*.scss', gulp.series(buildStylesTask));
 }));
 
 
