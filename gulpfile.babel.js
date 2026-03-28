5e21748f909fc2d1643db8fb8a7e88e4f59cf635
dakur <dev@kurowsti.cz>
2026-03-28 15:10:11 +0000

    even more simplify gulp build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,7 @@
 'use strict';
 
 import gulp from 'gulp';
-import taskList from 'gulp-task-listing';
 import autoprefixer from 'gulp-autoprefixer';
-import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
 import gulpSass from 'gulp-sass';
 import * as sassPackage from 'sass';
@@ -18,7 +16,7 @@ const stylesPath = {
 
 // STYLES
 
-const buildStylesTask = (cb) => {
+const buildTask = (cb) => {
 	const files = [
 		{
 			distPath: stylesPath.dist, // folder to save the compiled css file into
@@ -45,16 +43,14 @@ const buildStylesTask = (cb) => {
 		}
 
 		gulp.src(file.sourceFileName)
-			.pipe(plumber())
 			.pipe(rename(file.distFileName)) // must be before sass to keep correct file names between css and sourcemap
 			.pipe(sourcemaps.init())
 			.pipe(sass({
 				noCache: true,
 				outputStyle: (process.env.NODE_ENV === 'production' ? 'compressed' : 'expanded')
-			}))
+			}).on('error', sass.logError))
 			.pipe(autoprefixer()) // retrieves from `.browserslistrc`
 			.pipe(sourcemaps.write('.'))
-			.pipe(plumber.stop())
 			.pipe(gulp.dest(file.distPath))
 			.on('end', () => {
 				if (parseInt(i) === (files.length-1)) { // call only on last pipe
@@ -64,25 +60,7 @@ const buildStylesTask = (cb) => {
 	}
 };
 
-gulp.task('build:styles', gulp.series(buildStylesTask));
-gulp.task('watch:styles', gulp.series(buildStylesTask, () => {
-	gulp.watch(stylesPath.src + '/**/*.scss', gulp.series(buildStylesTask));
+gulp.task('build', gulp.series(buildTask));
+gulp.task('watch', gulp.series(buildTask, () => {
+	gulp.watch(stylesPath.src + '/**/*.scss', gulp.series(buildTask));
 }));
-
-
-// HELPERS
-
-// todo: finish USER in docker, maybe yarn will be then available for www-data
-// todo: then try these things
-
-gulp.task('help', taskList);
-
-gulp.task('watch', gulp.parallel(
-	'watch:styles',
-));
-
-gulp.task('build', gulp.parallel(
-	'build:styles',
-));
-
-gulp.task('default', gulp.series('build'));
