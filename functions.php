5ccdefc6bf70324920488f5fc986018874b950c7
dakur <dev@kurowsti.cz>
2026-08-18 09:11:53 +0000

    main nav

--- functions.php
+++ functions.php
@@ -14,6 +14,11 @@ add_action( 'after_setup_theme', function() {
     add_theme_support( 'core-block-patterns' );
     add_theme_support( 'automatic-feed-links' );
     add_theme_support( 'title-tag' );
+    add_theme_support( 'menus' );
+
+    register_nav_menus( [
+        'header' => __( 'Hlavní navigace', 'brontosaurus-fse' ),
+    ] );
 } );
 
 add_action( 'wp_enqueue_scripts', function() {
